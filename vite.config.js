a7597e0377b1cb5be8cccaf66581a99105a424df
madnifm347 <madnifm347@gmail.com>
2024-12-01 12:09:29 +0000

    start

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: "/expense-tracker/",
+  base: "/Expense-Eracker/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
